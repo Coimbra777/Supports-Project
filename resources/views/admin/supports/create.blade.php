8b70019937778892b33fb3e9f984cc43a3687056
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-04-21 13:54:44 -0300

    feat: validações dos campos de formulário

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -2,9 +2,17 @@
 
 <a href="{{ route('supports.index') }}">Voltar</a>
 
+@if ($errors->any())
+<ul>
+  @foreach ($errors->all() as $error)
+  <li>{{ $error }}</li>
+  @endforeach
+</ul>
+@endif
+
 <form action="{{ route('supports.store') }}" method="POST">
   <input type="hidden" name="_token" value="{{ csrf_token() }}">
-  <input type=" text" placeholder="Assunto" name="subject">
-  <textarea name="body" id="" cols="30" rows="10" placeholder="Descrição"></textarea>
+  <input type=" text" placeholder="Assunto" name="subject" value="{{old('subject')}}">
+  <textarea name="body" id="" cols="30" rows="10" placeholder="Descrição">{{old('body')}}</textarea>
   <button type="submit">Enviar</button>
 </form>
