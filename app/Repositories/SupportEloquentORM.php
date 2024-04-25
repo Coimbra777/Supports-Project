3cc3d3155b8d6289128b2dd23b64f5417889c0ff
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-04-25 15:45:01 -0300

    feat: implementação do repository

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -2,18 +2,39 @@
 
 use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
+use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
+  public function __construct(protected Support $model)
+  {
+  }
   public function getAll(string $filter = null): array
   {
-    return [];
+    return $this->model
+      ->where(function ($query) use ($filter) {
+        if ($filter) {
+          $query->where('subject', $filter);
+          $query->orWhere('body', 'like', $filter);
+        }
+      })
+      ->all()
+      ->toArray();
   }
 
   public function findOne(string $id): stdClass | null
   {
-    return null;
+
+    $support = (object) $this->model->find($id);
+    if (!$support) {
+      return null;
+    }
+    return (object) $support->toArray();
+  }
+  public function delete(string $id): void
+  {
+    $this->model->find($id)->delete();
   }
 
   public function new(CreateSupportDTO $dto): stdClass
@@ -24,9 +45,5 @@ class SupportEloquentORM implements SupportRepositoryInterface
   public function update(UpdateSupportDTO $dto): stdClass | null
   {
     return null;
-  }
-
-  public function delete(string $id): void
-  {
   }
 }
