831ac43ce783c52217b5ae7fdf0e97a6f3564538
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-04-25 14:44:22 -0300

    feat: adição repositorie

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -1,43 +1,32 @@
 <?php
 
-namespace App\Services;
-
 use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
 use App\Repositories\SupportRepositoryInterface;
-use stdClass;
 
-class SupportService
+class SupportEloquentORM implements SupportRepositoryInterface
 {
-  public function __construct(
-    protected SupportRepositoryInterface $repository
-  ) {
-  }
   public function getAll(string $filter = null): array
   {
-    return $this->repository->getAll($filter);
+    return [];
   }
 
   public function findOne(string $id): stdClass | null
   {
-    return $this->repository->findOne($id);
+    return null;
   }
 
   public function new(CreateSupportDTO $dto): stdClass
   {
-    return $this->repository->new($dto);
+    return new stdClass();
   }
-
-
 
   public function update(UpdateSupportDTO $dto): stdClass | null
   {
-    return $this->repository->update($dto);
+    return null;
   }
-
 
   public function delete(string $id): void
   {
-    $this->repository->delete($id);
   }
 }
